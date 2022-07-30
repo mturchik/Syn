9dfaeb241ea1c2c9fd779e41ec9c0a0ded0b51ee
mturchik <mturchik@batteriesplus.com>
2022-07-29 21:06:00 -0500

    Add/Update SignalR and Sql dependcies

--- Syn/Data/Migrations/20220730005228_mssql.azure_migration_397.Designer.cs
+++ Syn/Data/Migrations/20220730005228_mssql.azure_migration_397.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Syn.Data;
 
@@ -11,9 +12,10 @@ using Syn.Data;
 namespace Syn.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220730005228_mssql.azure_migration_397")]
+    partial class mssqlazure_migration_397
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Syn/Data/Migrations/20220730005228_mssql.azure_migration_397.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Syn.Data.Migrations
+{
+    public partial class mssqlazure_migration_397 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
