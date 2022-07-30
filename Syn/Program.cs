86d91a7fdc1d08ddbd02eb4193873fc0b4545155
mturchik <mturchik@batteriesplus.com>
2022-07-29 21:53:31 -0500

    Enable SignalR and StickyServerMode

--- Syn/Program.cs
+++ Syn/Program.cs
@@ -1,22 +1,29 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Azure.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Syn.Areas.Identity;
 using Syn.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+// Services
+builder.Services.AddSingleton<WeatherForecastService>();
+//Blazor
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSignalR().AddAzureSignalR(options =>
+{
+    options.ServerStickyMode = ServerStickyMode.Required;
+});
 
 var app = builder.Build();
 
